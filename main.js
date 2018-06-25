23f8d8b4e3bc4c8015503735f74b51e78544e9fb
dkreidler <dkreidler@gmail.com>
2018-06-24 22:19:04 -0400

    Adding in loops and closures to deal with closure issues in loops

--- main.js
+++ main.js
@@ -5,10 +5,31 @@ btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+for (var i = 1; i <= 5; i++) {
+    var newImage = document.createElement('img');
+    newImage.setAttribute('src', "images/pic" + i + ".jpg");
+    thumbBar.appendChild(newImage);
+    var src = newImage.getAttribute('src');
 
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
-  thumbBar.appendChild(newImage);
+    (function() {
+        var eventHandlers = [];
+
+        function createEventHandlers(i) {
+            return function() {
+                //console.log('My value is ' + i);
+            };
+        }
+        for (var i = 0; i <= 5; i++) {
+            eventHandlers[i] = createEventHandlers(i);
+        }
+        for (var j = 0; j <= 5; j++) {
+            eventHandlers[j];
+        }
+        //var j = i;
+        console.log('Image source is: ' + src);
+    }());
+
+    // newImage.addEventListener('click', displayedImage.setAttribute('src', src));
+}
 
 /* Wiring up the Darken/Lighten button */
-
