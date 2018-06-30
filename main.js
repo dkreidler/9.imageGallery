8c27cc45ec87b71a882bcffab268c0e65ffa30e4
dkreidler <dkreidler@gmail.com>
2018-06-30 10:35:07 -0400

    Trying to create eventHandler in loop

--- main.js
+++ main.js
@@ -11,25 +11,24 @@ for (var i = 1; i <= 5; i++) {
     thumbBar.appendChild(newImage);
     var src = newImage.getAttribute('src');
 
-    (function() {
-        var eventHandlers = [];
+    var eventHandlers = [];
 
-        function createEventHandlers(i) {
-            return function() {
-                //console.log('My value is ' + i);
-            };
-        }
-        for (var i = 0; i <= 5; i++) {
-            eventHandlers[i] = createEventHandlers(i);
-        }
-        for (var j = 0; j <= 5; j++) {
-            eventHandlers[j];
-        }
-        //var j = i;
-        console.log('Image source is: ' + src);
-    }());
-
-    // newImage.addEventListener('click', displayedImage.setAttribute('src', src));
+    function createEventHandlers(i) {
+        return function() {
+            //console.log('My value is ' + i);
+        };
+    }
+    for (var i = 0; i <= 5; i++) {
+        eventHandlers[i] = createEventHandlers(i);
+    }
+    for (var j = 0; j <= 5; j++) {
+        eventHandlers[j];
+    }
+    //var j = i;
+    console.log('Image source is: ' + src);
 }
+
+// newImage.addEventListener('click', displayedImage.setAttribute('src', src));
+
 
 /* Wiring up the Darken/Lighten button */
